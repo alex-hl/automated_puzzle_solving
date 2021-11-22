a3e06397b7acb1886a72d56118e48ca76f6a9bf2
seine78 <94844500+seine78@users.noreply.github.com>
2021-11-22 11:49:13 -0500

    Add files via upload

---
+++ solver.py
@@ -0,0 +1,188 @@
+"""
+CSC148, Winter 2021
+Assignment 2: Automatic Puzzle Solver
+==============================
+This code is provided solely for the personal and private use of
+students taking the CSC148 course at the University of Toronto.
+Copying for purposes other than this use is expressly prohibited.
+All forms of distribution of this code, whether as given or with
+any changes, are expressly prohibited.
+
+Authors: Diane Horton, Jonathan Calver, Sophia Huynh,
+         Maryam Majedi, and Jaisie Sin.
+
+All of the files in this directory are:
+Copyright (c) 2021 Diane Horton, Jonathan Calver, Sophia Huynh,
+                   Maryam Majedi, and Jaisie Sin.
+
+=== Module Description ===
+
+This module contains the abstract Solver class and its two subclasses, which
+find solutions to puzzles, step by step.
+"""
+
+from __future__ import annotations
+
+from typing import List, Optional, Set
+
+# You may remove this import if you don't use it in your code.
+from adts import Queue
+
+from puzzle import Puzzle
+
+
+class Solver:
+    """"
+    A solver for full-information puzzles. This is an abstract class
+    and purely provides the interface for our solve method.
+    """
+
+    # You may NOT change the interface to the solve method.
+    # Note the optional parameter seen and its type.
+    # Your implementations of this method in the two subclasses should use seen
+    # to keep track of all puzzle states that you encounter during the
+    # solution process.
+    def solve(self, puzzle: Puzzle,
+              seen: Optional[Set[str]] = None) -> List[Puzzle]:
+        """
+        Return a list of puzzle states representing a path to a solution of
+        <puzzle>. The first element in the list should be <puzzle>, the
+        second element should be a puzzle that is in <puzzle>.extensions(),
+        and so on. The last puzzle in the list should be such that it is in a
+        solved state.
+
+        In other words, each subsequent item of the returned list should take
+        the puzzle one step closer to a solution, which is represented by the
+        last item in the list.
+
+        Return an empty list if the puzzle has no solution.
+
+        <seen> is either None (default) or a set of puzzle states' string
+        representations, whose puzzle states can't be any part of the path to
+        the solution.
+        """
+        raise NotImplementedError
+
+
+# Your solve method MUST be a recursive function (i.e. it must make
+# at least one recursive call to itself)
+# You may NOT change the interface to the solve method.
+class DfsSolver(Solver):
+    """"
+    A solver for full-information puzzles that uses
+    a depth first search strategy.
+    """
+
+    def solve(self, puzzle: Puzzle,
+              seen: Optional[Set[str]] = None) -> List[Puzzle]:
+        """
+        Return a list of puzzle states representing a path to a solution of
+        <puzzle>. The first element in the list should be <puzzle>, the
+        second element should be a puzzle that is in <puzzle>.extensions(),
+        and so on. The last puzzle in the list should be such that it is in a
+        solved state.
+
+        In other words, each subsequent item of the returned list should take
+        the puzzle one step closer to a solution, which is represented by the
+        last item in the list.
+
+        Return an empty list if the puzzle has no solution.
+
+        <seen> is either None (default) or a set of puzzle states' string
+        representations, whose puzzle states can't be any part of the path to
+        the solution.
+        """
+        if puzzle.fail_fast():
+            return []
+        elif puzzle.is_solved():
+            return [puzzle]
+        else:
+            x = [puzzle]
+            grids = puzzle.extensions()
+            if seen is None:
+                seen = set()
+            seen.add(str(puzzle))
+            num = len(x)
+            for i in grids:
+                if not i.fail_fast() and not str(i) in seen:
+                    seen.add(str(i))
+                    if not x[-1].is_solved():
+                        x = x[0:num] + self.solve(i, seen)
+                seen.add(str(i))
+            if x[-1].is_solved():
+                return x
+            else:
+                return []
+
+
+# Hint: You may find a Queue useful here.
+class BfsSolver(Solver):
+    """"
+    A solver for full-information puzzles that uses
+    a breadth first search strategy.
+    """
+
+    def solve(self, puzzle: Puzzle,
+              seen: Optional[Set[str]] = None) -> List[Puzzle]:
+        """
+        Return a list of puzzle states representing a path to a solution of
+        <puzzle>. The first element in the list should be <puzzle>, the
+        second element should be a puzzle that is in <puzzle>.extensions(),
+        and so on. The last puzzle in the list should be such that it is in a
+        solved state.
+
+        In other words, each subsequent item of the returned list should take
+        the puzzle one step closer to a solution, which is represented by the
+        last item in the list.
+
+        Return an empty list if the puzzle has no solution.
+
+        <seen> is either None (default) or a set of puzzle states' string
+        representations, whose puzzle states can't be any part of the path to
+        the solution.
+        """
+        q = Queue()
+        if seen is None:
+            seen = set()
+        seen.add(str(puzzle))
+        x = puzzle
+        y = [puzzle]
+        while not q.is_empty() or x is not None and not x.is_solved():
+            seen.add(str(x))
+            for i in x.extensions():
+                if not i.fail_fast() and not str(i) in seen and i != y[0]:
+                    seen.add(str(i))
+                    q.enqueue(i)
+                    y.append(i)
+                seen.add(str(i))
+            x = q.dequeue()
+        if y[-1].is_solved() or (len(y) >= 2 and y[-2].is_solved()):
+            if y[-1].is_solved():
+                fl = [y[-1]]
+                a = y[-1]
+            else:
+                fl = [y[-2]]
+                a = y[-2]
+            for i in reversed(y):
+                if a in i.extensions():
+                    fl.append(i)
+                    a = i
+            return list(reversed(fl))
+        else:
+            return []
+
+
+if __name__ == "__main__":
+    import python_ta
+
+    python_ta.check_all(config={'pyta-reporter': 'ColorReporter',
+                                'allowed-io': [],
+                                'allowed-import-modules': ['doctest',
+                                                           'python_ta',
+                                                           'typing',
+                                                           '__future__',
+                                                           'puzzle',
+                                                           'adts'],
+                                'disable': ['E1136'],
+                                'max-attributes': 15}
+                        )
